81e292563354cd27bfb240d24d42ca9470693ee4
Will Hawkins <hawkinsw@obs.cr>
2023-11-13 10:16:34 -0500

    fixup! Initial wasm commit.
    Make wasm ubpf test use new oo API.

--- wasm/main.ts
+++ wasm/main.ts
@@ -1,11 +1,8 @@
 import {
-  create_runtime,
-  exec_ubpf,
   generate_ubpf_dispatch_system,
-  set_unwind_index,
   load_runtime,
-  load_ubpf,
   malloc,
+  Ubpf,
 } from "./ubpf.ts";
 
 import Context from "https://deno.land/std/wasi/snapshot_preview1.ts";
@@ -38,12 +35,62 @@ function gather_bytes(
   a3: bigint,
   a4: bigint,
 ): bigint {
-  const result=((a0 & BigInt(0xff))<<BigInt(32)) |
-               ((a1 & BigInt(0xff))<<BigInt(24)) |
-               ((a2 & BigInt(0xff))<<BigInt(16)) |
-               ((a3 & BigInt(0xff))<< BigInt(8)) |
-               ((a4 & BigInt(0xff))<< BigInt(0))
-  return result
+  const result = ((a0 & BigInt(0xff)) << BigInt(32)) |
+    ((a1 & BigInt(0xff)) << BigInt(24)) |
+    ((a2 & BigInt(0xff)) << BigInt(16)) |
+    ((a3 & BigInt(0xff)) << BigInt(8)) |
+    ((a4 & BigInt(0xff)) << BigInt(0));
+  return result;
+}
+
+function generate_memfrob(memory: ArrayBuffer) {
+  return function (
+    a0: bigint,
+    a1: bigint,
+    a2: bigint,
+    a3: bigint,
+    a4: bigint,
+  ): bigint {
+    const view8 = new DataView(memory, Number(a0), Number(a1));
+
+    for (let i = 0; i < a1; i++) {
+      let v = view8.getUint8(i);
+      v ^= 42;
+      view8.setUint8(i, v);
+    }
+
+    return BigInt(0);
+  };
+}
+
+function generate_strcmp(memory: ArrayBuffer) {
+  return function (
+    a0: bigint,
+    a1: bigint,
+    a2: bigint,
+    a3: bigint,
+    a4: bigint,
+  ): bigint {
+    const a0view8 = new DataView(memory, Number(a0));
+    const a1view8 = new DataView(memory, Number(a1));
+
+    for (let i = 0; i < memory.byteLength; i++) {
+      if (
+        a0view8.getUint8(i) == 0 &&
+        a1view8.getUint8(i) == 0
+      ) {
+        return BigInt(0);
+      }
+      if (a0view8.getUint8(i) == a1view8.getUint8(i)) {
+        continue;
+      }
+      if (a0view8.getUint8(i) < a1view8.getUint8(i)) {
+        return BigInt(-1);
+      }
+      return BigInt(1);
+    }
+    return BigInt(0);
+  };
 }
 
 async function main() {
@@ -71,18 +118,17 @@ async function main() {
   const [wasm, memory] = result;
   const memory_buffer = memory.buffer;
 
-  const vm = create_runtime(wasm);
+  const ubpf = new Ubpf(wasm);
+
   let initial_program_memory_d = new Uint8Array(0);
 
   const argv = Deno.args;
-  for (let i = 0; i < argv.length; i++) {
-    if (argv[i] != "--" && i == 0) {
-      // By spec we have a memory block to read!
-      const initial_raw_memory = argv[i].replaceAll(" ", "");
-      initial_program_memory_d = decodeHex(initial_raw_memory);
-    }
-    // We don't care about any other CLI options at this point.
+  if (argv[0] != "--" && argv[0].length != 0) {
+    // By spec we have a memory block to read!
+    const initial_raw_memory = argv[0].replaceAll(" ", "");
+    initial_program_memory_d = decodeHex(initial_raw_memory);
   }
+  // We don't care about any other CLI options at this point.
 
   const raw_program_decoder = new TextDecoder();
   const raw_program = raw_program_decoder.decode(
@@ -95,10 +141,14 @@ async function main() {
     raw_program.replaceAll(" ", "").replaceAll("\n", ""),
   );
 
-  set_unwind_index(wasm, vm, 5);
-  ubpf_register(wasm, vm, 0, gather_bytes);
-  ubpf_register(wasm, vm, 5, unwind);
-  ubpf_register(wasm, vm, 2, no_op);
+  ubpf.SetUnwindIndex(5);
+  const memfrob = generate_memfrob(memory_buffer);
+  const strcmp = generate_strcmp(memory_buffer);
+  ubpf_register(wasm, ubpf, 0, gather_bytes);
+  ubpf_register(wasm, ubpf, 1, memfrob);
+  ubpf_register(wasm, ubpf, 2, no_op);
+  ubpf_register(wasm, ubpf, 4, strcmp);
+  ubpf_register(wasm, ubpf, 5, unwind);
 
   const program_bytes_loc = malloc(wasm, program_bytes_d.length);
   const program_bytes = new Uint8Array(
@@ -117,17 +167,15 @@ async function main() {
   program_memory.set(initial_program_memory_d);
   program_bytes.set(program_bytes_d);
 
-  const [load_result, load_error] = load_ubpf(wasm, vm, program_bytes);
+  const [load_result, load_error] = ubpf.LoadProgram(program_bytes);
   if (!load_result) {
     console.log(`Failed to load code: ${load_error}`);
-    Deno.exit(1)
+    Deno.exit(1);
   }
   if (debug) {
     console.log(`load_result: ${load_result}`);
   }
-  const exec_actual_result = exec_ubpf(
-    wasm,
-    vm,
+  const exec_actual_result = ubpf.Execute(
     memory_buffer,
     program_memory.byteOffset,
     program_memory.length,
@@ -137,7 +185,7 @@ async function main() {
   } else {
     console.log(`${exec_actual_result.toString(16)}`);
   }
-  Deno.exit(0)
+  Deno.exit(0);
 }
 
 await main();
